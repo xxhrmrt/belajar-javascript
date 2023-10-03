9ff805a951b542fe7c07376c376f096415efc087
xxhrmrt <146801826+xxhrmrt@users.noreply.github.com>
2023-10-03 17:36:20 +0700

    Create TP2.js

---
+++ TP2.js
@@ -0,0 +1,9 @@
+var firstName = "Satria Dewanto"
+var lastName = "Indraloka"
+var age = 20
+var isMarried = false
+
+console.log(`Nama Depan\t\t: ${firstName}`)
+console.log(`Nama Belakang\t\t: ${lastName}`)
+console.log(`Umur\t\t\t: ${age}`)
+console.log(`Status Pernikahan\t: ${isMarried}`)
