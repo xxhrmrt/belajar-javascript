ed804f89938e59de6b499e04587d9bb7a4048cee
Satria Harimurti <146801826+xxhrmrt@users.noreply.github.com>
2023-10-16 15:56:09 +0700

    Add files via upload

---
+++ TP3 - 1.js
@@ -0,0 +1,9 @@
+const restaurant = {
+    name: "Warteg Bahari Benhil",
+    city: "Jakarta Pusat",
+    "favorite drink": "Ice Tea",
+    "favorite food": "Kentang Mustofa",
+    isVegan: false,
+};
+
+console.table(restaurant)
---
+++ TP3 - 2.js
@@ -0,0 +1,18 @@
+const restaurant = [
+    {
+        name: "Warteg Bahari Benhil",
+        city: "Jakarta Pusat",
+        "favorite drink": "Es Teh Manis",
+        "favorite food": "Kentang Mustofa",
+        isVegan: false,
+    },
+    {
+        name: "Mie Ayam Pakde",
+        city: "Jakarta Pusat",
+        "favorite drink": "Es Teh Manis",
+        "favorite food": "Mie Ayam Yamin",
+        isVegan: false,
+    }
+];
+
+console.table(restaurant)
