1cb29e34798668862a95483fd5fe8712b560c1f2
Satria Harimurti <146801826+xxhrmrt@users.noreply.github.com>
2024-01-28 22:46:06 +0700

    Pemrograman Berbasis Platform - Tugas 15

---
+++ Tugas 15 - latihan_restfulAPI/belajar_nodejs_restful_api.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE belajar_nodejs_restful_api;
+
+USE belajar_nodejs_restful_api;
+
+SHOW TABLES;
+
+DESCRIBE users;
+
+DESCRIBE contacts;
+
+DESCRIBE addresses;
+
+SELECT * FROM users;
+
+SELECT * FROM contacts;
+
+SELECT * FROM addresses;
+
+SELECT * FROM _prisma_migrations;
+
